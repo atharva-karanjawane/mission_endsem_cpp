d5b587528111f87d40c48f9592dc01dbdac85213
atharva-karanjawane <141766418+atharva-karanjawane@users.noreply.github.com>
2024-11-19 11:35:49 +0530

    Create method_overloading.cpp

---
+++ method_overloading.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;    
+}
+
+void sum(int a, int b,int c){
+    cout<<a+b+c;    
+}
+
+int main() {
+    
+sum(3,4,7);
+
+    return 0;
+}
