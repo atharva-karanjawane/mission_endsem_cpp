5076a079227d43b9b43f8ebec353e7adcad19f54
Atharva Karanjawane <gayatrikaranjawaneathu@gmail.com>
2024-11-19 20:21:15 +0530

    Create type_casting.cpp

---
+++ type_casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// Implicit Type Casting
+float x = 4.5;
+
+// Explicit Type Casting 
+int sum = int(x) + 4; 
+
+cout<<sum;
+    return 0;
+}
